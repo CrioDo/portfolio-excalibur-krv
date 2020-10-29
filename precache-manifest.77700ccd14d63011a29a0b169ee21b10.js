3b476545cb7b9804e858dd87c0eea9221497adef
CrioDo <rahul@criodo.com>
2020-10-29 21:04:28 +0530

    Updates

---
+++ precache-manifest.77700ccd14d63011a29a0b169ee21b10.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac76f9fad331ddf209a3d519d140912",
+    "url": "/portfolio-excalibur-krv/index.html"
+  },
+  {
+    "revision": "ca8fb4dde35218d4cdec",
+    "url": "/portfolio-excalibur-krv/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "afb47b79655892c17c60",
+    "url": "/portfolio-excalibur-krv/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "ca8fb4dde35218d4cdec",
+    "url": "/portfolio-excalibur-krv/static/js/2.5a9ae3c0.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/portfolio-excalibur-krv/static/js/2.5a9ae3c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afb47b79655892c17c60",
+    "url": "/portfolio-excalibur-krv/static/js/main.8ce39ac0.chunk.js"
+  },
+  {
+    "revision": "e5b3ac43be3a312ce63e",
+    "url": "/portfolio-excalibur-krv/static/js/runtime-main.0f2bbadf.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/portfolio-excalibur-krv/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/portfolio-excalibur-krv/static/media/crio-logo.63be71d0.svg"
+  }
+]);
